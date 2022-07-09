ae320e3b98d398dec49c280f1f1e483421909dea
julia <julian.risler@gmail.com>
2022-07-09 09:55:04 +0200

    [change] - moving dummy data from ReservationRestController to ReservationService

---
+++ src/main/java/com/sda/springdemojavaee14/controller/rest/ReservationRestController.java
@@ -0,0 +1,28 @@
+package com.sda.springdemojavaee14.controller.rest;
+
+import com.sda.springdemojavaee14.entity.Reservation;
+import com.sda.springdemojavaee14.service.ReservationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/api")
+public class ReservationRestController {
+
+    private final ReservationService reservationService;
+
+    public ReservationRestController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping("/reservations")
+    public List<Reservation> getAllReservations() {
+        log.info("getting all reservations");
+    }
+}

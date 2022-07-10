4776d4faebd50fb37fc69fdaf0151632ae788d04
julia <julian.risler@gmail.com>
2022-07-10 10:19:00 +0200

    [add] - Person class with builder annotation - Main class with builder explanation

--- src/main/java/com/sda/springdemojavaee14/homework/annotation/Main.java
+++ src/main/java/com/sda/springdemojavaee14/homework/annotation/Main.java
@@ -16,5 +16,15 @@ public class Main {
         System.out.println("Are those two references referring to the same object?: " + (me == anotherMe));
         System.out.println("is this the same person? -" + me.equals(anotherMe));
 
+        System.out.println("are equals: " + me.equals("haha"));
+
+        Person meAgain = new Person("julian", "r.", 3);
+        // builder is better than all args constructor - because you can see more:)
+        Person meAgainWithBuilder = Person.builder()
+                .age(33)
+                .surname("r.")
+                .name("julian")
+                .build();
+
     }
 }
